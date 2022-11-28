67f18ec66c34ff182faba22564079ad597c39cad
Jorim Soika <jorim.soika@gmx.de>
2022-11-28 21:57:41 +0800

    included static files

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 })
